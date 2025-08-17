5c5fc6dda70f3a0977faf94f8497519ce3f79ff7
cahves1 <athoschaveslopesdealmeida@gmail.com>
2025-08-17 20:07:16 -0300

    WTF is webpack????

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development', // Adicione esta linha
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        name: '[path][name].[ext]'
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [],
+    resolve: {
+        extensions: ['.js', '.css']
+    }
+};
